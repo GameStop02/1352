d44779c80ab4a5609ce51a1d6443a98ba2c01422
gamestopp02 <soapgrovee@gmail.com>
2026-09-21 19:42:56 +0100

    Add files via upload

--- css/main.js
+++ css/main.js
@@ -105,13 +105,13 @@ async function doJb() {
       const kpatches_u8 = new Uint8Array(kpatches_buf);
       kernel_patches(kpatches_u8);
 
-      const bin_rsp = await fetch("goldhen_2.4b18.10.bin");
+      const bin_rsp = await fetch("GoldHEN_v2.4b18.12.bin");
       const bin_buf = await bin_rsp.arrayBuffer();
       const bin_u8 = new Uint8Array(bin_buf);
       load_bin(bin_u8);
     }
 
-    msgs.innerHTML = "GoldHEN v2.4b18.10...تم تحميل";
+    msgs.innerHTML = "GoldHEN v2.4b18.12...تم تحميل";
     logger.info("===END===");
   } catch (e) {
     msgs.innerHTML = "...فشل التحميل! أعد تشغيل جهازك";
